d9272362650d16cf954eb767aa9e2931b62dcbd8
dimitris <iracleous@gmail.com>
2023-06-01 10:38:26 +0300

    Add project files.

---
+++ Meb-Nbg-2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Meb_Nbg_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Meb-Nbg-2023/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Meb_Nbg_2023.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string Description { get; set; } = "";
+    }
+}
